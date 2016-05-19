9daf319df5ff6adcdf47daa0f6add7d2190fb4b6
endyvirgo <endyvirgo@gmail.com>
2016-05-19 14:53:58 +0700

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  DATN_Scale
+//
+//  Created by KhanhNguyen on 4/13/16.
+//  Copyright © 2016 KhanhNguyen. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    private var _force: CGFloat = 0.0
+    var force: CGFloat {get {return _force}}
+    var maximumForce: CGFloat = 8.0
+    @IBOutlet weak var gramLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let firstTouch = touches.first {
+            if #available(iOS 9.2, *) {
+                if traitCollection.forceTouchCapability == UIForceTouchCapability.Available {
+                    maximumForce = min(firstTouch.maximumPossibleForce, maximumForce)
+                    if firstTouch.force >= maximumForce {
+                        let max = (maximumForce/firstTouch.maximumPossibleForce) * 337
+                        gramLabel.text = "\(max) grams"
+                    } else {
+                        let max = (maximumForce/firstTouch.maximumPossibleForce) * 337
+                        let _force = firstTouch.force/maximumForce
+                        let grams = _force * max
+                        let roundGrams = Int(grams)
+                        gramLabel.text = "\(roundGrams) grams"
+                    }
+                }
+            }
+        }
+    }
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        gramLabel.text = " 0 gram"
+    }
+    
+    
+    
+}
